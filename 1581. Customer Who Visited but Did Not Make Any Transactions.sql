27e2bf9232a43194396aed741a3efdffeee8e195
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-02 13:41:24 +0200

    Delete 1581. Customer Who Visited but Did Not Make Any Transactions.sql

--- 1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++
@@ -1,5 +0,0 @@
-select CUSTOMER_ID, count(visits.visit_id) as count_no_trans
-from visits left join Transactions
-on visits.visit_id = Transactions.visit_id
-where transaction_id is null
-group by customer_id;
