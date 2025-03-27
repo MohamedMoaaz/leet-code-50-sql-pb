78787d1040147726ff99bd9342b69738eef02c2f
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-27 16:48:14 +0200

    Update Delete Duplicate Emails.sql

--- Advanced String Functions-Regex-Clause/Delete Duplicate Emails.sql
+++ Advanced String Functions-Regex-Clause/Delete Duplicate Emails.sql
@@ -1,1 +1,9 @@
-
+DELETE FROM person 
+WHERE id IN (
+    SELECT id FROM (
+        SELECT id, 
+               ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS rn
+        FROM person
+    )
+    WHERE rn > 1
+);
