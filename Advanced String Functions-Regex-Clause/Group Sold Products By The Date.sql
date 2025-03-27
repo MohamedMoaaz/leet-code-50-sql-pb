3bab562b7bb3d0e5b692163ff892f41523dddadc
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-27 16:49:05 +0200

    Update Group Sold Products By The Date.sql

--- Advanced String Functions-Regex-Clause/Group Sold Products By The Date.sql
+++ Advanced String Functions-Regex-Clause/Group Sold Products By The Date.sql
@@ -1,1 +1,6 @@
-
+select to_char(sell_date, 'YYYY-MM-DD') sell_date,
+count(product) num_sold,
+LISTAGG(product, ',') within group (order by product) products
+from (select distinct sell_date, product from activities)
+group by sell_date
+order by sell_date
