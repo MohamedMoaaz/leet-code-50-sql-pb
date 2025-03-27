0b1b73c34649f16c682b5833ca982fc77c6a0ffe
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-27 17:06:27 +0200

    Update List the Products Ordered in a Period.sql

--- Advanced String Functions-Regex-Clause/List the Products Ordered in a Period.sql
+++ Advanced String Functions-Regex-Clause/List the Products Ordered in a Period.sql
@@ -1,1 +1,9 @@
-
+with cte as
+    (select product_id, sum(unit) unit
+    from orders
+    where to_char(order_date, 'YYYY-MM') = '2020-02'
+    group by product_id
+    having sum(unit) >= 100)
+select product_name, unit
+from products p join cte c
+on p.product_id = c.product_id
