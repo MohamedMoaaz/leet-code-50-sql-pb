2d034bad1e724c727a42f8967f0f2e3df3554102
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-27 18:19:23 +0200

    Update Second Highest Salary.sql

--- Advanced String Functions-Regex-Clause/Second Highest Salary.sql
+++ Advanced String Functions-Regex-Clause/Second Highest Salary.sql
@@ -1,1 +1,8 @@
-
+WITH cte AS (
+    SELECT id, salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rank
+    FROM employee
+)
+SELECT COALESCE((
+    SELECT MAX(salary) FROM cte WHERE rank = 2
+), NULL) AS SecondHighestSalary
+FROM dual;
